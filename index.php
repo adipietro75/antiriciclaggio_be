2b6fa3901c9e002f0b9b3de102932d89be6da9ed
adipietro75 <57714414+adipietro75@users.noreply.github.com>
2019-11-27 10:16:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ header("Access-Control-Allow-Credentials: true");
 
 define ('PROJECT_DIRECTORY', dirname(__DIR__));
 
-require PROJECT_DIRECTORY . '/gestione_messaggi_607/vendor/autoload.php';
-require PROJECT_DIRECTORY . '/gestione_messaggi_607/config.php';
+require PROJECT_DIRECTORY . '/antiriciclaggio_be/vendor/autoload.php';
+require PROJECT_DIRECTORY . '/antiriciclaggio_be/config.php';
 
 global $appConfig;
 
 $app = new \Slim\App($appConfig['appconfig']);
 
-include PROJECT_DIRECTORY . '/gestione_messaggi_607/src/Bootstrap/containers.php';
-include PROJECT_DIRECTORY . '/gestione_messaggi_607/src/Bootstrap/routes.php';
+include PROJECT_DIRECTORY . '/antiriciclaggio_be/src/Bootstrap/containers.php';
+include PROJECT_DIRECTORY . '/antiriciclaggio_be/src/Bootstrap/routes.php';
 
 // START !
 $app->run();
