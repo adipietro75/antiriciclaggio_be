269ec66624629834b0254a2b8d9f8a7d828ab709
adipietro75 <57714414+adipietro75@users.noreply.github.com>
2019-12-04 18:46:03 +0100

    implementati contenzioso.inc model_decaduti.inc

--- src/Bootstrap/routes.php
+++ src/Bootstrap/routes.php
@@ -12,5 +12,15 @@ $app->group('', function () {
     $this->get('/getDatiTrasmessiCSV', mainAppController::class . ':getDatiTrasmessiCSV');
     $this->get('/selezioneGiochi', mainAppController::class . ':selezioneGiochi');
     $this->get('/giochiDisponibiliPerConcessione', mainAppController::class . ':giochiDisponibiliPerConcessione');
+	
+    //rc01_contenzioso.inc
+    $this->get('/controlla_valori1', mainAppController::class . ':controlla_valori1');
+    $this->get('/controlla_valori2', mainAppController::class . ':controlla_valori2');	
+    $this->get('/controlla_valori3', mainAppController::class . ':controlla_valori3'); 
     
+	//rc01_model_decaduti.inc
+	$this->get('/CaricaDati', mainAppController::class . ':CaricaDati');	
+	
+	//rc01_decadenza.inc
+	
 })->add($container['AuthenticationMiddleware']);
